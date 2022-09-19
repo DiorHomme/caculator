82eddbae0f4df68b7bd8c73e3c4f0363d8b15695
song <song@218.38.137.28>
2022-09-19 19:06:02 +0900

    사칙연산 기호에 따라 계산 가능하도록 설정해

--- src/calculator.java
+++ src/calculator.java
@@ -3,17 +3,29 @@ import java.util.Scanner;
 public class calculator {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		
 		System.out.println("첫 번째 입력 값 : ");
 		int first = scanner.nextInt();
 		System.out.println(first);
 
+		System.out.println("사칙연산 기호 : ");
+		String symbol = scanner.next();
+		System.out.println(symbol);
+		
 		System.out.println("두번째 입력 값 : ");
 		int second = scanner.nextInt();
 		System.out.println(second);
 		
-		System.out.println("덧셈 : " + (first + second));
-		System.out.println("뺄샘 : " + (first - second));
-		System.out.println("곱샘 : " + (first * second));
-		System.out.println("나눗셈 : " + (first / second));
+		if (symbol.equals("+")) {
+			System.out.println("덧셈 : " + (first + second));		
+		} else if (symbol.equals("-")) {
+			System.out.println("뺄샘 : " + (first - second));
+		} else if (symbol.equals("*")) {
+			System.out.println("곱샘 : " + (first * second));
+		} else if (symbol.equals("/")) {
+			System.out.println("나눗셈 : " + (first / second));
+		} else {	
+			System.out.println("사칙연산 기호가 아닙니다.");
+	}
 	}
 }
