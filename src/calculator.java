714b97e281871f34c281173d2f9a6c1644568586
song <song@218.38.137.28>
2022-09-19 18:42:07 +0900

    inital commit

---
+++ src/calculator.java
@@ -0,0 +1,9 @@
+
+public class calculator {
+	public static void main(String[] args) {
+		System.out.println("덧셈 : " + (3+4));
+		System.out.println("뺄샘 : " + (4-3));
+		System.out.println("곱샘 : " + (4*3));
+		System.out.println("나눗셈 : " + (4 / 2));
+	}
+}
